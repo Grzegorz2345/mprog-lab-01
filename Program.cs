f0f74604c6210f2ff0d5efc8e000c77497454536
Stefan Batory <afgawegfdawe@xd.pl>
2021-11-29 12:57:07 +0100

    test zmian

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@ namespace mprog_lab_01
 {
     class Program
     {
+        public static void wypisz_pierwsze(int x){
+            for(int i=3;i<=x;i++){
+                for(int j=2;j<=x/2;j++){
+                    if(i%j==0)
+                    break;
+                    else if(j==x/2)
+                    Console.WriteLine(i);
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            wypisz_pierwsze(10);
         }
     }
 }
