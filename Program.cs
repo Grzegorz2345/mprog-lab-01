5aaa585ad6243f609b7acd2c535a4762608b0923
Stefan Batory <afgawegfdawe@xd.pl>
2021-12-06 12:49:23 +0100

    dodanie obsługi wyjątków

--- Program.cs
+++ Program.cs
@@ -4,20 +4,26 @@ namespace mprog_lab_01
 {
     class Program
     {
-        public static void wypisz_pierwsze(int x){
-            for(int i=3;i<=x;i++){
-                for(int j=2;j<=x/2;j++){
-                    if(i%j==0)
-                    break;
-                    else if(j==x/2)
-                    Console.WriteLine(i);
-                }
-            }
-        }
+        
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            wypisz_pierwsze(10);
+        {StreamReader sr=null;
+            try{
+           
+           sr= new StreamReader("/home/doc");
+           Console.WriteLine(sr.ReadToEnd());
+           }
+           catch(FileNotFoundException ex){
+               Console.WriteLine($"File not found{ex.FileName}")
+           }
+           catch(Exception ex){
+               Console.WriteLine(ex.Message);
+               Console.WriteLine(ex.StackTrace);
+               
+           }
+           finally{
+               sr?.Close();
+           }
+           
         }
     }
 }
